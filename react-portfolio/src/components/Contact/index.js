8266562233f3b58606765da1290ab3ed971c16ed
Justin <stutlerjustin@gmail.com>
2024-10-15 17:04:42 -0400

    Contact Me(Working)
    Contact Page contains a contact me section that sends an email with a name, email address, subject, and message to my email

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,7 +1,99 @@
+import { useEffect, useState } from 'react'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
+// import env vars
+const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+
 const Contact = () => {
-    return <></>
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 5000);
+
+        // Cleanup function to clear the timeout
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        console.log(userId, templateId, serviceId);  // Check if these values are correct
+
+        emailjs
+            .sendForm(serviceId, templateId, form.current, userId)
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
+
+    return (
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I am interested in freelance as well as employment opportunities.
+                        Feel free to message me with any inquiries below.
+                    </p>
+                    <div className="contact-form">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Contact;
+export default Contact
