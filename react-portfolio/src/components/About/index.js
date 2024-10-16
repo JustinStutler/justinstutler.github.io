9ac793d03ce35d7e1fa2b2b447c964f7f4f76220
Justin <stutlerjustin@gmail.com>
2024-10-16 17:41:27 -0400

    Added Work in progress text

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,7 +1,10 @@
 import './index.scss'
 
 const About = () => {
-    return <></>
+    return <><div style={{ textAlign: 'center', color: '#ffd700', fontSize: '24px', marginTop: '50px' }}>
+        Work in Progress... Come Back Soon
+    </div>
+    </>
 }
 
 export default About;
--- react-portfolio/src/components/Projects/index.js
+++ react-portfolio/src/components/Projects/index.js
@@ -1,7 +1,10 @@
 import './index.scss'
 
 const Projects = () => {
-    return <></>
+    return <><div style={{ textAlign: 'center', color: '#ffd700', fontSize: '24px', marginTop: '50px' }}>
+        Work in Progress... Come Back Soon
+    </div>
+    </>
 }
 
 export default Projects;
